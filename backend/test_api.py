4da696d4c3e4fa2bf00a551dd7876bb7bf789b9f
kaqui-null <emanuelgomes24.08@gmail.com>
2025-04-28 20:35:13 -0300

    Atualizado o teste

--- backend/test_api.py
+++ backend/test_api.py
@@ -4,10 +4,18 @@ import requests
 BASE_URL = "http://127.0.0.1:8000"
 
 new_item = {
-    "name":"Test item"
+    "email":"Testitem@gmail.com"
 }
+new_item_get = {"email": "TestItemForGet@gmail.com"}
 
 response = requests.post(f"{BASE_URL}/items/", json=new_item)
 
+get_response = requests.post(f"{BASE_URL}/items/", json=new_item)
+
 print("STATUS_CODE:", response.status_code)
 print("Response Json:", response.json())
+
+get_response = requests.get(f"{BASE_URL}/items/")
+
+print("GET Status:", get_response.status_code)
+print("All Items:", get_response.json())
