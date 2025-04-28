cbf7dc3f770c77d6f968e31b88bdc17e871cf174
alocinny <alocinnygame@gmail.com>
2025-04-28 20:09:34 -0300

    update -> name para email

--- backend/back.py
+++ backend/back.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import List
 import sqlite3
 
@@ -8,12 +8,12 @@ app = FastAPI()
 #banco de dados
 conn = sqlite3.connect('database.db', check_same_thread=False)
 cursor = conn.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL)")
+cursor.execute("CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT,email TEXT NOT NULL UNIQUE)")
 conn.commit()
 
 #modelos
 class Item(BaseModel):
-    name: str
+    email: EmailStr
 
 class ItemResponse(Item):
     id: int
@@ -21,27 +21,26 @@ class ItemResponse(Item):
 #create    
 @app.post("/items", response_model=ItemResponse)
 def create_item(item: Item):
-    cursor.execute('INSERT INTO items (name) VALUES (?)', (item.name,))
+    cursor.execute('INSERT INTO items (email) VALUES (?)', (item.email,))
     conn.commit()
     item_id = cursor.lastrowid
-    return ItemResponse(id=item_id, name=item.name)
+    return ItemResponse(id=item_id, email=item.email)
 
 #read all
 @app.get("/items", response_model=List[ItemResponse])
 def read_items():
-    cursor.execute('SELECT id, name FROM items')
+    cursor.execute('SELECT id, email FROM items')
     rows = cursor.fetchall()
-    return [ItemResponse(id=row[0], name=row[1]) for row in rows]
-
+    return [ItemResponse(id=row[0], email=row[1]) for row in rows]
 
 #read One
 @app.get("/items/{item_id}", response_model=ItemResponse)
 def read_item(item_id: int):
-    cursor.execute('SELECT id, name FROM items WHERE id = ?', (item_id,))
+    cursor.execute('SELECT id, email FROM items WHERE id = ?', (item_id,))
     row = cursor.fetchone()
     if row is None:
         raise HTTPException(status_code=404, detail="Item not found")
-    return ItemResponse(id=row[0], name=row[1])
+    return ItemResponse(id=row[0], email=row[1])
 
 #update
 @app.put("/items/{item_id}", response_model=ItemResponse)
@@ -50,9 +49,9 @@ def update_item(item_id: int, item: Item):
     if cursor.fetchone() is None:
         raise HTTPException(status_code=404, detail="Item not found")
     
-    cursor.execute('UPDATE items SET name = ? WHERE id = ?', (item.name, item_id))
+    cursor.execute('UPDATE items SET email = ? WHERE id = ?', (item.email, item_id))
     conn.commit()
-    return ItemResponse(id=item_id, name=item.name)
+    return ItemResponse(id=item_id, email=item.email)
 
 #delete
 @app.delete("/items/{item_id}")
